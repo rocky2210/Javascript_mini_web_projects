467fb29be8acfe1d78762c2e3765c8be25e35f9f
rocky2210 <lilavathra@gmail.com>
2024-02-15 17:37:16 +0530

    Coin Toss

---
+++ 011_Coin_Toss/script.js
@@ -0,0 +1,55 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coin");
+let flipButton = document.querySelector("#flip-button");
+let resetButton = document.querySelector("#reset-button");
+let flipSound = new Audio('coin.mp3')
+
+
+// Function for Sound effect 
+function playFlipSound() {
+    flipSound.play();
+}
+
+flipButton.addEventListener("click",function(){
+    let i = Math.floor(Math.random() * 2);
+    coin.style.animation = "none";
+
+    if(i){
+        setTimeout(function(){
+            coin.style.animation = "spin-heads 2s forwards";
+        },100);
+        heads++;
+    }else{
+        setTimeout(function(){
+            coin.style.animation = "spin-tails 2s forwards";
+        },100);
+        tails++
+    }
+
+    setTimeout(updateStats, 2000);
+    disableButton();
+    playFlipSound();
+});
+
+// Function for update points
+function updateStats(){
+    document.querySelector("#head-count").textContent = `Heads: ${heads}`;
+    document.querySelector("#tail-count").textContent = `Tails: ${tails}`;
+}
+
+// Function for disablebutton
+function disableButton(){
+    flipButton.disabled = true;
+    setTimeout(function(){
+        flipButton.disabled = false
+    }, 3000);
+}
+
+// Function for reset
+resetButton.addEventListener("click",function(){
+    coin.style.animation = "none";
+    heads = 0;
+    tails = 0;
+    updateStats();
+});
